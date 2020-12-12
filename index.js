8ebda6cf9b52fdd699df9d5e4147fc597db1c5f1
tbanish <tombanish@gmail.com>
2020-12-12 17:53:10 -0500

    commit tree d59a3cd15d745b972f2219791710a1a1193c70cf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+  return array.filter((s => s.toUpperCase() === string.toUpperCase()))
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter((s => s.split("")[0].toUpperCase() === string.split("")[0].toUpperCase()))
+}
+
+function matchName(array, string) {
+  return array.filter((s) => s.name.toUpperCase() === string.toUpperCase())
+}
